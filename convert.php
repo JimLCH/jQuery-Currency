455f6e495de3bbfd8093872c9b965c4c9dcc5734
Lim Chun Hong <106853846+JimLCH@users.noreply.github.com>
2022-06-04 12:24:48 +0800

    Update convert.php

--- convert.php
+++ convert.php
@@ -14,3 +14,10 @@ if (is_numeric($converted_amount)) {
         echo $amount;
     }
 ?>
+
+$from = urlencode($_POST['from']);
+  $to = urlencode($_POST['to']);
+  $get = file_get_contents("https://www.google.com/finance/converter?a=$amount&from=$from&to=$to");
+  $get = explode("<span class=bld>",$get);
+  $get = explode("</span>",$get[1]);  
+  $converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
